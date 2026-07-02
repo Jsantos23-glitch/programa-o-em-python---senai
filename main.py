c37efc5f6fe4d98ba84c4257b7f0749740c71f75
Jsantos23-glitch <rosantos2327@gmail.com>
2026-07-02 17:05:00 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import metodo
+
+# Atividade 1
+
+print("atividade 1:")
+print(metodo.atividade1())
+
+# Atividade 2 
+
+print("\natividade 2:")
+print(metodo.atividade2())
+
+# Atividade 3
+
+print("\natividade 3:")
+print(metodo.atividade3())
+
+# Atividade 4
+
+print("\natividade 4:")
+metodo.atividade4()
+
+# Atividade 5
+
+print("\natividade 5:")
+num = int(input("Digite um número positivo: "))
+print("Soma dos pares:", metodo.atividade5(num))
+
+# Atividade 6
+
+print("\natividade 6:")
+num = int(input("Digite um número para ver a tabuada: "))
+metodo.atividade6(num)
+
+# Atividade 7
+
+print("\natividade 7:")
+metodo.atividade7()
